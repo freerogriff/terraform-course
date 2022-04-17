a48e8dd93f3d126e4f3346d040641adf6be24d0a
freerogriff <rogriff@msn.com>
2022-04-16 23:46:54 -0700

    fixed backend.tf

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,9 +1,7 @@
-/*
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-xx70dpnh"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-*/
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-xx70dpnh"
+#    key    = "terraform.tfstate"
+#    region = "us-west-2"
+#  }
+#}
