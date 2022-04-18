e40acf7180e081758b60978be27b4007213176de
freerogriff <rogriff@msn.com>
2022-04-18 04:25:56 -0700

    fixed vpc in security group

--- jenkins-packer-demo/securitygroup.tf
+++ jenkins-packer-demo/securitygroup.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "jenkins-securitygroup" {
 }
 
 resource "aws_security_group" "app-securitygroup" {
-  vpc_id      = aws_default_vpc.main.id
+  vpc_id      = aws_vpc.main.id
   name        = "app-securitygroup"
   description = "security group that allows ssh and all egress traffic"
   egress {
